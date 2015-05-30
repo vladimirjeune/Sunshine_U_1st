42b35417c2dc4af5c4b9c5b795b559c067e86976
vladimirjeune <randyjeune@gmail.com>
2015-05-29 21:35:23 -0700

    Answer: Match answer on how to bring up Settings in option menu

--- src/com/vrj/udacity/sunshine/app/DetailActivity.java
+++ src/com/vrj/udacity/sunshine/app/DetailActivity.java
@@ -39,8 +39,9 @@ public class DetailActivity extends ActionBarActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
-			Intent intent = new Intent(this, SettingsActivity.class);
-			startActivity(intent);
+
+			// We are not expecting an extra so just call like this
+			startActivity(new Intent(this, SettingsActivity.class));
 			return true;
 		} 
 		return super.onOptionsItemSelected(item);
--- src/com/vrj/udacity/sunshine/app/MainActivity.java
+++ src/com/vrj/udacity/sunshine/app/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends ActionBarActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
-			Intent intent = new Intent(this, SettingsActivity.class);
-			startActivity(intent);
+			// We do not need an Intent variable for anything so just call in function
+			startActivity(new Intent(this, SettingsActivity.class));
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
