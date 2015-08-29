058d1e91173d463f5a262aab36db166e5ae1242d
vladimirjeune <randyjeune@gmail.com>
2015-08-29 00:27:07 -0700

    You: Do Update and Insert

--- src/com/vrj/udacity/sunshine/app/data/WeatherProvider.java
+++ src/com/vrj/udacity/sunshine/app/data/WeatherProvider.java
@@ -284,17 +284,34 @@ public class WeatherProvider extends ContentProvider {
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         // Student: Start by getting a writable database
+    	final SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+    	final int match = sUriMatcher.match(uri);
+    	int retRows = 0;
 
         // Student: Use the uriMatcher to match the WEATHER and LOCATION URI's we are going to
         // handle.  If it doesn't match these, throw an UnsupportedOperationException.
+    	switch (match) {
+    		case WEATHER:
+    			retRows += db.delete(WeatherContract.WeatherEntry.TABLE_NAME, selection, selectionArgs);
+    			break;
+    		case LOCATION:
+    			retRows += db.delete(WeatherContract.LocationEntry.TABLE_NAME, selection, selectionArgs);
+    			break;
+    		default:
+    			throw new UnsupportedOperationException("Unknown uri: " + uri);
+    	}
 
         // Student: A null value deletes all rows.  In my implementation of this, I only notified
         // the uri listeners (using the content resolver) if the rowsDeleted != 0 or the selection
         // is null.
-        // Oh, and you should notify the listeners here.
+        // Oh, and you should notify the listeners here.    	
+    	if ((selection == null) || (retRows != 0) ){
+    		// Tell the ContentResolver about the change
+    		getContext().getContentResolver().notifyChange(uri, null);
+    	}
 
         // Student: return the actual rows deleted
-        return 0;
+        return retRows;
     }
 
     /**
@@ -316,7 +333,26 @@ public class WeatherProvider extends ContentProvider {
             Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         // Student: This is a lot like the delete function.  We return the number of rows impacted
         // by the update.
-        return 0;
+    	final SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+    	final int match = sUriMatcher.match(uri);
+    	int retRows = 0;
+    	
+    	switch(match) {
+    		case WEATHER:
+    			retRows += db.update(WeatherContract.WeatherEntry.TABLE_NAME, values, selection, selectionArgs);
+    			break;
+    		case LOCATION:
+    			retRows += db.update(WeatherContract.LocationEntry.TABLE_NAME, values, selection, selectionArgs);
+    			break;
+    		default:
+    			throw new UnsupportedOperationException("Unknown uri: " + uri);
+    	}
+
+    	if (retRows > 0) {
+    		getContext().getContentResolver().notifyChange(uri, null);
+    	}
+    	
+        return retRows;
     }
 
     @Override
