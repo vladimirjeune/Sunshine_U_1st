095f8c4f53539c92c2e1705a63c074afcaa462fa
vladimirjeune <randyjeune@gmail.com>
2015-09-05 01:27:56 -0700

    Use String[] of Projection in ForecastFragment CursorLoader, and ForecastAdapter

--- src/com/vrj/udacity/sunshine/app/ForecastFragment.java
+++ src/com/vrj/udacity/sunshine/app/ForecastFragment.java
@@ -31,6 +31,36 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 	private final static int FORECAST_LOADER_ID = 0;
 	private ForecastAdapter mForecastAdapter = null;
 	
+	private static final String[] FORECAST_COLUMNS = {
+		// In this case the id needs to be fully qualified with a table name, since
+		// the content provider joins the location & weather tables in the background
+		// (both have an _id column)
+		// On the one hand, that's annoying.  On the other, you can search the weather table
+		// using the location set by the user, which is only in the Location table.
+		// So the convenience is worth it.
+		WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+		WeatherContract.WeatherEntry.COLUMN_DATE,
+		WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+		WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+		WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+		WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING,
+		WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
+		WeatherContract.LocationEntry.COLUMN_COORD_LAT,
+		WeatherContract.LocationEntry.COLUMN_COORD_LONG
+	};
+
+	// These indices are tied to FORECAST_COLUMNS.  If FORECAST_COLUMNS changes, these
+	// must change.  Projections
+	static final int COL_WEATHER_ID = 0;
+	static final int COL_WEATHER_DATE = 1;
+	static final int COL_WEATHER_DESC = 2;
+	static final int COL_WEATHER_MAX_TEMP = 3;
+	static final int COL_WEATHER_MIN_TEMP = 4;
+	static final int COL_LOCATION_SETTING = 5;
+	static final int COL_WEATHER_CONDITION_ID = 6;
+	static final int COL_COORD_LAT = 7;
+	static final int COL_COORD_LONG = 8;
+	
 	public ForecastFragment() {
 	}
 
@@ -132,7 +162,7 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 		return new CursorLoader(
 				getActivity(), 
 				weatherForLocationUri, 
-				null, 
+				ForecastFragment.FORECAST_COLUMNS, 
 				null, 
 				null, 
 				sortOrder);  // ASC usually
