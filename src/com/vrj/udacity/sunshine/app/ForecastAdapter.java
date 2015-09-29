78320aa39f3f84df39c99fe8c64521e1810f4a7b
vladimirjeune <randyjeune@gmail.com>
2015-09-28 19:13:07 -0700

    Make it so the correct item view type is selected.  Today layout is different than regular layout.

--- src/com/vrj/udacity/sunshine/app/ForecastAdapter.java
+++ src/com/vrj/udacity/sunshine/app/ForecastAdapter.java
@@ -23,6 +23,21 @@ import com.vrj.udacity.sunshine.app.data.WeatherContract;
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+	
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+    
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
+    }
+	
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
@@ -50,14 +65,26 @@ public class ForecastAdapter extends CursorAdapter {
                 " - " + highAndLow;
     }
 
-    /*
-        Remember that these views are reused as needed.
+    /**
+     * Remember that these views are reused as needed.
+     * Choose the right viewType so we can have the correct layout for the
+     *  data.
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
 
-        return view;
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+        // Determine layoutId from viewType
+        
+        if (viewType == VIEW_TYPE_FUTURE_DAY ) {
+        	layoutId = R.layout.list_item_forecast;
+        } else {
+        	layoutId = R.layout.list_item_forecast_today;
+        }
+        
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     /*
